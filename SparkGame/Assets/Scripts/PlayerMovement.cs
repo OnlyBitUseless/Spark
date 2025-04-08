01b4f0c4f93a832ddbcc16e13f1a8f44d5767f47
OnlyBitUseless <daniel.hei@proton.me>
2025-04-08 15:04:03 +0300

    Kääntymissäätöä vaatii vielä korjausta

--- SparkGame/Assets/Scripts/PlayerMovement.cs
+++ SparkGame/Assets/Scripts/PlayerMovement.cs
@@ -6,22 +6,29 @@ public class PlayerMovement : MonoBehaviour
 {   
     public float speed = 10;
 
-    private Rigidbody tankBase;
-
+    private Rigidbody tank_base;
 
     private float movementX;
     private float movementY;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        tankBase = GetComponent<Rigidbody>();
+        tank_base = GetComponent<Rigidbody>();
     }
     // Update is called once per frames
     void Update()
     {
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
-        tankBase.AddForce(speed * movement);
+        tank_base.AddForce(speed * movement);
     }
+
+    void OnRotate (InputValue rotateValue)
+    {
+        float rotation = rotateValue.Get();
+        transform.Rotate(0,rotateValue,0);
+    }
+
     void OnMove (InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
