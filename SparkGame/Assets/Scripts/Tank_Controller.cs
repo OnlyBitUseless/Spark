f90258a36ececb7546cd92b06b588a0bfcbf976b
OnlyBitUseless <daniel.hei@proton.me>
2025-04-14 15:18:40 +0300

    Max rotation and max speed

--- SparkGame/Assets/Scripts/Tank_Controller.cs
+++ SparkGame/Assets/Scripts/Tank_Controller.cs
@@ -7,9 +7,9 @@ using UnityEngine;
          #region Variables
          [Header("Movement properties")]
          public float tank_speed = 15f;
-         public float tank_rotation_speed = 20f;
- 
-         private float movement;
+         public float tank_max_speed = 100f;
+         public float tank_rotation_speed = 200f;
+         public float tank_rotation_at_max_speed = 15f;
  
          private Rigidbody rb;
          private Tank_Inputs input;
@@ -34,11 +34,17 @@ using UnityEngine;
          #region CustomMethods
          protected virtual void HandleMovement()
          {
-            Vector3 movement = transform.forward * input.ForwardInput;
-             rb.AddForce(movement * tank_speed);
+            float forward_speed = Vector3.Dot(transform.forward, rb.linearVelocity);
+            float speed_factor = Mathf.InverseLerp(0, tank_max_speed, Mathf.Abs(forward_speed));
+
+            float current_speed = Mathf.Lerp(tank_speed, 0, speed_factor);
+            float current_rotation = Mathf.Lerp(tank_rotation_speed, tank_rotation_at_max_speed, speed_factor);
+
+            Vector3 movement = transform.forward * input.ForwardInput * current_speed;
+            rb.AddForce(movement);
  
-             Quaternion target_rotation = transform.rotation * Quaternion.Euler(Vector3.up * (tank_rotation_speed * input.RotationInput * Time.deltaTime));
-             rb.MoveRotation(target_rotation);
+            Quaternion target_rotation = transform.rotation * Quaternion.Euler(Vector3.up * (current_rotation* input.RotationInput * Time.deltaTime));
+            rb.MoveRotation(target_rotation);
          }
          #endregion
      }
