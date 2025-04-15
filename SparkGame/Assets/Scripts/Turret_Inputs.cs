831660bfea860b616c19a7b9b9070aae6bea6ce8
OnlyBitUseless <daniel.hei@proton.me>
2025-04-15 09:45:54 +0300

    Turret rotation

--- SparkGame/Assets/Scripts/Turret_Inputs.cs
+++ SparkGame/Assets/Scripts/Turret_Inputs.cs
@@ -8,25 +8,34 @@ namespace Tank
         public Transform kansi;
         public Transform piippu;
         private Camera mainCamera;
+
         void Awake()
         {
             mainCamera = Camera.main;
         }
+
         public LayerMask groundLayer;
+
         public float turret_rotation_speed = 150f;
         private Rigidbody rb;
         private Vector3 targetPoint;
+        private float angle;
+
         public Vector3 TargetPoint
         {
             get { return targetPoint; }
         }
+
         private Tank_Inputs input;
+        
         #endregion
         
+        #region BuildinMethods
         void Start()
         {
             rb = GetComponent<Rigidbody>();
             input = GetComponent<Tank_Inputs>();
+            InvokeRepeating("rotateToMouse", 2.0f, 0.1f);
         }
 
         
@@ -34,10 +43,16 @@ namespace Tank
         // Update is called once per frame
         void FixedUpdate()
         {
-            rotateToMouse();
+            float maxAngleThisFrame = turret_rotation_speed * Time.deltaTime;
+            float clampedAngle = Mathf.Clamp(angle, -maxAngleThisFrame, maxAngleThisFrame);
+            piippu.RotateAround(kansi.position, Vector3.up, clampedAngle);
+            angle = angle-clampedAngle;
         }
 
+        #endregion
+
         #region CustomMethods  
+
         protected virtual void rotateToMouse() 
         {
             Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
@@ -54,15 +69,9 @@ namespace Tank
                 Vector3 fromDirection = piippu.position - kansi.position;
                 Vector3 toDirection = direction;
 
-                float angle = Vector3.SignedAngle(fromDirection, toDirection, Vector3.up);
-                
-
-                float maxAngleThisFrame = turret_rotation_speed * Time.deltaTime;
-                float clampedAngle = Mathf.Clamp(angle, -maxAngleThisFrame, maxAngleThisFrame);
-
-
-                piippu.RotateAround(kansi.position, Vector3.up, clampedAngle);
+                angle = Vector3.SignedAngle(fromDirection, toDirection, Vector3.up);
             }
+
         }
         #endregion
     }
